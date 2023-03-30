6138e6cdb8bf0433648be7a3c8f650c8c4bdd3e2
David Carlyn <davidecarlyn@gmail.com>
2023-03-30 10:30:15 -0400

    Copying over Imageomics related projects to this repository

---
+++ src/butterflies_transformation/src/options.py
@@ -0,0 +1,18 @@
+import yaml
+
+class Options:
+    def __init__(self, configs):
+        for key in configs:
+            setattr(self, key, configs[key])
+
+    def __str__(self):
+        out = "=======Options=======\n"
+        for key in self.__dict__.keys():
+            out += f"{key} => {self.__dict__[key]}\n"
+        out += "====================="
+        return out
+
+def load_config(path):
+    with open(path) as f:
+        configs = yaml.safe_load(f)
+    return Options(configs)
