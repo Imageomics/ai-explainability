74721f82220188204ac85a0a7f180018e2b0ef84
David Carlyn <davidecarlyn@gmail.com>
2023-04-25 12:54:40 -0400

    Able to train resnet50 to 80% on cub

--- src/class_cvae/datasets.py
+++ src/class_cvae/datasets.py
@@ -6,10 +6,11 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 class CUB(Dataset):
-    def __init__(self, root, train=True, transform=None):
+    def __init__(self, root, train=True, bbox=False, transform=None):
         super().__init__()
 
         self.transform=transform
+        self.use_bbox = bbox
 
         ids = []
         with open(os.path.join(root, "train_test_split.txt")) as f:
@@ -17,6 +18,13 @@ class CUB(Dataset):
                 id, is_train = line.split()
                 if int(is_train) == int(train):
                     ids.append(id)
+
+        self.bboxs = []
+        with open(os.path.join(root, "bounding_boxes.txt")) as f:
+            for line in f.readlines():
+                id, x, y, width, height = line.split()
+                if id not in ids: continue
+                self.bboxs.append((float(x), float(y), float(x)+float(width), float(y)+float(height)))
 
         self.img_paths = []
         self.img_lbls = []
@@ -39,6 +47,8 @@ class CUB(Dataset):
         lbl = self.img_lbls[idx]
 
         img = Image.open(path).convert('RGB')
+        if self.use_bbox:
+            img = img.crop(self.bboxs[idx])
         if self.transform:
             img = self.transform(img)
 
--- src/class_cvae/train_classifier.py
+++ src/class_cvae/train_classifier.py
@@ -11,40 +11,70 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from torchvision.datasets import MNIST, ImageFolder
-from torchvision.transforms import ToTensor, Compose, RandomRotation, Resize
+import torchvision.transforms as T
+from torchvision.datasets import MNIST
+from torchvision.transforms.functional import pad
 
 from models import ImageClassifier, ResNet50
 from logger import Logger
 from datasets import CUB
 
-def load_data(dset, batch_size):
-    train_transform = Compose([
-        RandomRotation(45),
-        ToTensor()
+def cub_pad(img):
+    pad_size = max(max(img.height, img.width), 500)
+    y_to_pad = pad_size - img.height
+    x_to_pad = pad_size - img.width
+
+    top_to_pad = y_to_pad // 2
+    bottom_to_pad = y_to_pad - top_to_pad
+    left_to_pad = x_to_pad // 2
+    right_to_pad = x_to_pad - left_to_pad
+
+    return pad(img,
+        (left_to_pad, top_to_pad, right_to_pad, bottom_to_pad),
+        fill=tuple(map(lambda x: int(round(x * 256)), (0.485, 0.456, 0.406))))
+
+def load_data(dset, batch_size, use_bbox=False):
+    train_transform = T.Compose([
+        T.RandomRotation(45),
+        T.ToTensor()
     ])
 
-    test_transform = Compose([
-        ToTensor()
+    test_transform = T.Compose([
+        T.ToTensor()
     ])
 
     if dset == "cub":
-        train_transform = Compose([
-            Resize((256, 256)),
-            ToTensor()
+        flips_and_crop = [
+            T.RandomHorizontalFlip(),
+            T.RandomVerticalFlip(),
+        ]
+        if not use_bbox:
+            flips_and_crop.append(T.RandomCrop((375, 375)))
+
+        train_transform = T.Compose([
+            T.Lambda(cub_pad),
+            T.RandomOrder(flips_and_crop),
+            #T.RandomRotation(10),
+            #T.RandomPerspective(distortion_scale=0.25, p=0.5, interpolation=T.InterpolationMode.BILINEAR),
+            #T.Resize((256, 256)),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
-        test_transform = Compose([
-            Resize((256, 256)),
-            ToTensor()
+        test_transform = T.Compose([
+            T.Lambda(cub_pad),
+            #T.Resize((256, 256)),
+            T.CenterCrop((375, 375)),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
     if dset == "mnist":
         train_dset = MNIST(root="data", train=True, transform=train_transform, download=True)
         test_dset = MNIST(root="data", train=False, transform=test_transform)
     elif dset == "cub":
-        train_dset = CUB("/local/scratch/cv_datasets/CUB_200_2011/", train=True, transform=train_transform)
-        test_dset = CUB("/local/scratch/cv_datasets/CUB_200_2011/", train=False, transform=test_transform)
+        train_dset = CUB("/local/scratch/cv_datasets/CUB_200_2011/", train=True, bbox=use_bbox, transform=train_transform)
+        test_dset = CUB("/local/scratch/cv_datasets/CUB_200_2011/", train=False, bbox=use_bbox, transform=test_transform)
     train_dloader = DataLoader(train_dset, batch_size=batch_size, shuffle=True)
     test_dloader = DataLoader(test_dset, batch_size=batch_size, shuffle=False)
 
@@ -57,6 +87,7 @@ def get_args():
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--output_dir', type=str, default="output")
     parser.add_argument('--use_resnet', action="store_true", default=False)
+    parser.add_argument('--use_bbox', action="store_true", default=False)
     parser.add_argument('--exp_name', type=str, default="debug_img_classifier")
     parser.add_argument('--dset', type=str, default="mnist", choices=["mnist", "cub"])
     return parser.parse_args()
@@ -71,7 +102,7 @@ if __name__ == "__main__":
     elif args.dset == "mnist":
         num_classes = 10
         img_ch = 1
-    train_dloader, test_dloader = load_data(args.dset, args.batch_size)
+    train_dloader, test_dloader = load_data(args.dset, args.batch_size, use_bbox=args.use_bbox)
 
     classifier = ImageClassifier(num_classes)
     if args.use_resnet:
@@ -87,9 +118,10 @@ if __name__ == "__main__":
     class_loss_fn = nn.CrossEntropyLoss()
     params = list(classifier.parameters())
     optimizer = torch.optim.Adam(params, lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
     for epoch in range(args.epochs):
-        total_loss = 0
+        losses = []
         correct = 0
         total = 0
         classifier.train()
@@ -108,17 +140,17 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            losses.append(loss.item())
 
-            total_loss += loss.item()
-
-        logger.log(f"Epoch: {epoch+1} | Class Loss: {total_loss} | Train Accuracy: {round(correct/total, 4)}")
+        scheduler.step()
+        logger.log(f"Epoch: {epoch+1} | Class Loss: {np.mean(losses)} | Train Accuracy: {round(correct/total, 4)}")
 
         total_loss = 0
         correct = 0
         total = 0
         classifier.eval()
         with torch.no_grad():
-            for imgs, lbls in test_dloader:
+            for (imgs, lbls) in tqdm(test_dloader):
                 imgs = imgs.cuda()
                 lbls = lbls.cuda()
 
